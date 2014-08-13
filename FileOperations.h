c6aacc12cbbf0ed02175f2b0dc6c399e44535c4d
Bharathkumar Hegde <hegdebharathkumar@live.com>
2014-08-13 08:37:53 +0530

    Update FileOperations.h
    Added comments for clarity

--- FileOperations.h
+++ FileOperations.h
@@ -32,20 +32,32 @@ struct CVFS{
 	FT *myft;
 	BLOCK *free;
 };
+///////////////////////////////////////////////
+//Virtual File Initialization Functions
+//////////////////////////////////////////////
 
-//Virtual File Init Functions
+//can be used to get pointer to Virtual file system
 CVFS* getVFS();
+
+//intialize the VFS to certain size;
 void initVFS(int vfs_size);
 
-//Operartion API's
+
+
+
+//////////////////////////////////////////////
+//Operation API's
+/////////////////////////////////////////////
+
 //Creates a new file in VFS
 int createFile(const char* fileName);
 
-// offset 0-start -1=> end else specified offset returns 0 on success or error number.
+// offset 0-start -1=> end else specified offset
+// returns 0 on success or error number.
 int writeFile(const char* fileName,const void* buffer,int bufferSize,int offset);
 
-//reads charecters to buffer form the offset of the file
-//returns number of bytes read or -1 incase of error
+//reads characters to buffer form the offset of the file
+//returns number of bytes read or -1 in case of error
 int readFile(const char* filename,void* buffer,int bufferSize,int offset);
 
 //Renames already existing file in VFS
@@ -55,15 +67,16 @@ int renameFile(const char *ondName,const char *newName);
 int deleteFile(const char *fileName);
 
 //Utility functions for programmer
+
+//get a free block of specified size
 BLOCK *get_block(int block_size);
+//find min of two numbers
 int min(int a,int b);
+//make data block a fee block
 void add_to_free(BLOCK* add);
 
 //Utility functions for users
 void listFiles();
 void list_free_blocks();
 
-
-
-
-#endif /* FILEOPERATIONS_H_ */
+#endif
